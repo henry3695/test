8f97d986ecd768ffd512b6956238d20b1b7a2b7e
henry3695 <6102435@qq.com>
2023-08-16 10:40:31 +0800

    Update gethttp.py

--- gethttp.py
+++ gethttp.py
@@ -1,4 +1,5 @@
 import requests
+import base64
 headers = {
 'User-Agent':'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.87 Safari/537.36',
 }
@@ -6,3 +7,4 @@ url = 'https://ghproxy.com/https://raw.githubusercontent.com/Pawdroid/Free-serve
 #等价下面的
 response = requests.get(url, headers=headers).text
 print(response)
+print(base64.b64decode(response))
