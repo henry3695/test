e0aa4ec3938dac282bf1b972725f747d06ea23fb
henry3695 <6102435@qq.com>
2023-08-16 10:30:44 +0800

    asa

--- gethttp.py
+++ gethttp.py
@@ -2,7 +2,7 @@ import requests
 headers = {
 'User-Agent':'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.87 Safari/537.36',
 }
-url = 'https://httpbin.org/get'
+url = 'https://ghproxy.com/https://raw.githubusercontent.com/Pawdroid/Free-servers/main/sub'
 #等价下面的
 response = requests.get(url, headers=headers).text
 print(response)
