9ba9fdd437a4472282b41a2d3b7fb029a23bdb79
henry3695 <6102435@qq.com>
2024-08-08 19:45:16 +0800

    Add files via upload

---
+++ SQLiteDatabase.h
@@ -0,0 +1,32 @@
+﻿#ifndef SQLITEDATABASE_H
+#define SQLITEDATABASE_H
+
+#include <QObject>
+#include <QtSql>
+#include <QDebug>
+#include <QVector>
+class SQLiteDatabase : public QObject
+{
+    Q_OBJECT
+public:
+    explicit SQLiteDatabase(QObject *parent = nullptr);
+
+    bool openDatabase(const QString &dbName);
+    void close();
+       // 新增方法：检查表是否存在
+    bool isTableExists(const QString &tableName);
+    bool insertData(const QString& tableName, const QVariantMap& data);
+    bool RunSql(const QString& strSql);
+    bool GetTable(const QString& strSql,QVector<QStringList>& tbData);
+
+    QString GetLastError();
+
+   private:
+       QSqlDatabase db;
+       QString m_sError;
+
+       void SetError(const QString& sError);
+       
+};
+
+#endif // SQLITEDATABASE_H
